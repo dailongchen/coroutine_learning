2267a3af4f0f21fc35be52f3b94ac6f3bfae26e4
Solon Chen <Solon.Chen@mozy.com>
2018-04-18 17:39:17 +0800

    Change Crawler to accept RequestItem which can be inherited to add further requests.
    Change-Id: I7c61736ddee4807b33b8dd03def7dfe96d8d6ad5

--- my_crawler.py
+++ my_crawler.py
@@ -1,38 +1,90 @@
 import asyncio
 import aiohttp
 import re
+import sys
+import urllib
 
 
-class Clawler:
-    def __init__(self, max_redirect):
-        self._crawl_id = 0
+class RequestItem:
+    _item_id = 0
+
+    def __init__(self, url, max_redirect=10, chunk_size=4096):
+        RequestItem._item_id += 1
+        self.id = RequestItem._item_id
+
+        self.url = url
+        self.chunk_size = chunk_size
 
         self._max_redirect = max_redirect
-        self._url_queue = None
+        self.redirect_history = []
+
+        self._body_cache = b''
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def received(self, chunk):
+        self._body_cache += chunk
+
+    def text(self, encoding='utf-8'):
+        return self._body_cache.decode(encoding=encoding, errors='ignore')
+
+    def to_redirect(self, location):
+        self.redirect_history.append(location)
+        self.url = location
+        return len(self.redirect_history) <= self._max_redirect
+
+    def done(self, crawler: object) -> object:
+        pass
+
+
+class Crawler:
+    def __init__(self):
+        self._crawl_id = 0
+
+        self._semaphore = asyncio.Semaphore(10)
+
+        self._request_queue = None
         self._client_session = None
 
         self._seen_urls = set()
 
-    async def _save(self, data):
-        pass
+    async def _load(self, request_item):
+        with await self._semaphore:
+            url = request_item.url
+            if url in self._seen_urls:
+                return
 
-    async def _load(self, url):
-        if url in self._seen_urls:
-            return
+            print(url)
+            self._seen_urls.add(url)
 
-        print(url)
-        self._seen_urls.add(url)
+            try:
+                async with self._client_session.request('GET', url, allow_redirects=False, timeout=60) as response:
+                    if 'location' in response.headers:
+                        next_url = response.headers['location']
 
-        async with self._client_session.request('GET', url) as response:
-            data = await response.text()
-        return data
+                        if not next_url.startswith('http'):
+                            next_url = urllib.parse.urljoin(url, next_url)
+                            urllib.parse.quote(next_url)
 
-    def _fetch_url(self, text):
-        urls = set(re.findall(r'''(?i)href=["']([^\s"'<>]+)''',
-                              text))
-        for url in urls:
-            if url.startswith('http'):
-                self._url_queue.put_nowait(url)
+                        if request_item.to_redirect(next_url):
+                            self.add_request(request_item)
+                    elif response.status == 200:
+                        while True:
+                            chunk = await response.content.read(request_item.chunk_size)
+                            if not chunk:
+                                break
+                            request_item.received(chunk)
+                        request_item.done(self)
+            except asyncio.TimeoutError:
+                print('error: timeout. {}'.format(url))
+            except [aiohttp.client_exceptions.ClientResponseError,
+                    aiohttp.client_exceptions.ClientOSError] as exc:
+                print('error: {0}. {1}'.format(exc, url))
+            except:
+                exc_tuple = sys.exc_info()
+                print('error: {0}, {1}. {2}'.format(exc_tuple[0], exc_tuple[1], url))
+                raise
 
     async def _crawl(self):
         self._crawl_id += 1
@@ -40,34 +92,56 @@ class Clawler:
 
         while True:
             try:
-                url = await self._url_queue.get()
-                self._fetch_url(await self._load(url))
+                request = await self._request_queue.get()
+                await self._load(request)
             finally:
                 try:
-                    self._url_queue.task_done()
+                    self._request_queue.task_done()
                 except ValueError:
                     print('Clawler_{} is done'.format(this_crawl_id))
-                    await self._client_session.close()
 
-    async def run(self):
-        self._url_queue = asyncio.Queue()
+    async def close(self):
+        await self._client_session.close()
+
+    def add_request(self, request_item):
+        self._request_queue.put_nowait(request_item)
+
+    async def run(self, request_item):
+        self._request_queue = asyncio.PriorityQueue()
 
         tcp_connector = aiohttp.TCPConnector(verify_ssl=False)
-
         self._client_session = aiohttp.ClientSession(connector=tcp_connector)
 
         tasks = [asyncio.ensure_future(self._crawl()) for _ in range(5)]
 
-        await self._url_queue.put('https://www.google.com')
+        await self._request_queue.put(request_item)
 
-        await self._url_queue.join()
+        await self._request_queue.join()
         asyncio.gather(*tasks).cancel()
+
+        await self.close()
+
+
+class MyRequest(RequestItem):
+    def __init__(self, url):
+        RequestItem.__init__(self, url)
+
+    def _fetch_url(self):
+        urls = set(re.findall(r'''(?i)href=["']([^\s"'<>]+)''',
+                              self.text()))
+        for url in urls:
+            if url.startswith('http'):
+                yield url
+
+    def done(self, crawler: object) -> object:
+        for url in self._fetch_url():
+            crawler.add_request(MyRequest(url))
 
 
 if __name__ == '__main__':
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(Clawler(5).run())
+        loop.run_until_complete(Crawler().run(MyRequest('http://xkcd.org')))
         loop.close()
     except KeyboardInterrupt:
         pass
